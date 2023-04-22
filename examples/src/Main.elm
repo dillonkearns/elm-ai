0b2ef08b6a5e38933e56cb7a7f5fba2216988e79
Dillon Kearns <dillon@incrementalelm.com>
2023-04-22 12:53:32 -0700

    Add example project for TypeSolver.

---
+++ examples/src/Main.elm
@@ -0,0 +1,32 @@
+module Main exposing (..)
+
+import Dict
+import Http
+
+
+foo : String
+foo =
+    let
+        example2 : Result Http.Error a -> Result String a
+        example2 =
+            Debug.todo "REPLACE"
+
+        mergeDicts : Dict.Dict String (List String) -> Dict.Dict String (List String) -> Dict.Dict String (List String)
+        mergeDicts =
+            Debug.todo "example1"
+    in
+    ""
+
+
+type MyError
+    = MyError
+
+
+myErrorToString : MyError -> String
+myErrorToString myError =
+    "MyError"
+
+
+toMyError : Http.Error -> MyError
+toMyError error =
+    MyError
